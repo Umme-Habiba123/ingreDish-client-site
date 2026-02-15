eeb6e0d7a40c78f10b25d1e9a896dfb4b310a7ef
Umme-Habiba123 <habibaislammim@gmail.com>
2026-02-15 19:42:35 +0600

    last commit

--- src/shared/HeroSectionWithNavbar/HeroSectionWithNavbar.jsx
+++ src/shared/HeroSectionWithNavbar/HeroSectionWithNavbar.jsx
@@ -200,8 +200,8 @@ export default function HeroSectionWithNavbar() {
                 <Link href="/blog" className="text-white hover:text-gray-300 transition-colors body-text font-medium flex items-center gap-1">
                   Blog <ChevronDown className="w-4 h-4" />
                 </Link>
-                <Link href="/shop" className="text-white hover:text-gray-300 transition-colors body-text font-medium flex items-center gap-1">
-                  Shop <ChevronDown className="w-4 h-4" />
+                <Link href="/addproduct" className="text-white hover:text-gray-300 transition-colors body-text font-medium flex items-center gap-1">
+                 Add Product <ChevronDown className="w-4 h-4" />
                 </Link>
               </div>
 
